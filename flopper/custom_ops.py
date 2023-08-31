3fd38d83043bd343cc3d82dde158b2e0edaf2d7d
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2023-08-31 17:45:13 +0200

    fixed bug on linalg.solve

--- flopper/custom_ops.py
+++ flopper/custom_ops.py
@@ -19,9 +19,17 @@ def div_flop_jit(inputs: List[Any], outputs: List[Any]):
 def linalg_solve_flop_jit(inputs: List[Any], outputs: List[Any]):
     A_shape = get_shape(inputs[0])
     B_shape = get_shape(inputs[1])
-    n = A_shape[0]
-    m = B_shape[1] if len(B_shape) > 1 else 1
-    return int((2 / 3) * n**3) + (n**2 * m)
+
+    n = A_shape[-2]
+    m = B_shape[-1]
+
+    base_flops = int((2 / 3) * n**3) + (n**2 * m)
+
+    # Calculate broadcasted dimensions
+    broadcast_dims = [max(a, b) for a, b in zip(A_shape[:-2], B_shape[:-2])]
+    total_instances = np.prod(broadcast_dims)
+
+    return base_flops * total_instances
 
 
 FLOPPER_OPS = {
