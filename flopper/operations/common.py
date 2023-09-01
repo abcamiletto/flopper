1a854c2b0ec3aa75d9b9e6ed817babd8a5e34ef1
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2023-09-01 10:20:17 +0200

    added new operations

---
+++ flopper/operations/common.py
@@ -0,0 +1,20 @@
+from typing import Any, List
+
+import numpy as np
+from fvcore.nn.jit_handles import get_shape
+
+
+def mean_flop_jit(inputs, outputs):
+    input_shape = get_shape(inputs[0])
+    return np.prod(input_shape)
+
+
+def sum_flop_jit(inputs, outputs):
+    input_shape = get_shape(inputs[0])
+    return np.prod(input_shape)
+
+
+COMMON_OPS = {
+    "aten::mean": mean_flop_jit,
+    "aten::sum": sum_flop_jit,
+}
